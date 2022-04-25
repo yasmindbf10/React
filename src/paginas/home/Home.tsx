79053593a17f6b7498e2be02229fba841697843a
yasmindbf10 <yasmindbfdez@yahoo.com.br>
2022-04-25 19:41:36 -0300

    commit initial

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+        <h1 className='titulo'>Home</h1>
+        <img src="logo512.png" alt="imagem tela inicial" className='img'/>
+        </>
+    );
+}
+export default Home;
